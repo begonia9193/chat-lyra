128a28bb49c14abb1b7d2fde91c3e5c667c00edd
解语花 <windy9193@163.com>
2024-10-20 10:08:35 +0800

    refactor: code save, hotkey keys

--- src-tauri/src/hotkey.rs
+++ src-tauri/src/hotkey.rs
@@ -1,34 +1,17 @@
 use serde_json::Value;
 use crate::{CONFIG_STORE};
-use crate::translate::{ocr_translate, selection_translate};
-
-#[derive(Debug)]
-struct Hotkey {
-    key: &'static str,
-    handler: fn(),
-}
-
-static HOTKEYS: &[Hotkey] = &[
-    Hotkey {
-        key: "selection_translate",
-        handler: selection_translate,
-    },
-    Hotkey {
-        key: "ocr_translate",
-        handler: ocr_translate,
-    }
-];
 
 pub fn init_hotkey() {
     let config_store = CONFIG_STORE.get().unwrap();
 
     if let Some(Value::Object(hotkey_config)) = config_store.get("hotkey") {
-        for hotkey in HOTKEYS.iter() {
-            if let Some(Value::String(hotkey_value)) = hotkey_config.get(hotkey.key) {
+        let keys = vec!["selection_translate", "ocr_translate"];
+        for key in keys {
+            if let Some(Value::String(hotkey_value)) = hotkey_config.get(key) {
                 println!("{:?}", hotkey_value)
             }
         }
     }
 }
 
-pub fn registry() {}
+pub fn registry(key: &str, hotkey: &str, ) {}
