04e7ff5565bfd998c2d5bb69e9c041c0e60d3da9
begonia <jieyuhua9193@gmail.com>
2025-05-11 22:53:29 +0800

    fix: code save

--- src/workbench/App.tsx
+++ src/workbench/App.tsx
@@ -1,46 +1,11 @@
-import { WebviewWindow } from '@tauri-apps/api/webviewWindow';
 import './App.css';
-import { Icon } from '@/components/Icon';
-import { Button } from '@mui/material';
 import { MUIThemeProvider } from '@/provider/MUIThemeProvider';
+import Layout from './layout/Layout';
 
 function App() {
-  const getSettings = () => {
-    const settings = localStorage.getItem('settings');
-    console.log(settings);
-  };
-
-  const openSettings = async () => {
-    const settingWindow = await WebviewWindow.getByLabel('setting');
-    settingWindow?.setAlwaysOnTop(true);
-    settingWindow?.show();
-    settingWindow?.setFocus();
-    settingWindow?.setAlwaysOnTop(false);
-  };
-
   return (
     <MUIThemeProvider>
-      <div className='w-full h-full flex'>
-        <div className='w-full h-25px shrink-0 absolute top-0 left-0' data-tauri-drag-region></div>
-      <div className='w-240px h-full flex flex-col of-hidden b-r-1 b-solid b-line/60% pt-10px'>
-        <div className='h-50px b-b-1 b-solid b-line/60% flex items-center justify-center'>
-          LOGO
-        </div>
-        <div className='flex-1 h-full p-16px'>
-          123
-        </div>
-        <div className='h-50px px-16px flex items-center b-t-1 b-solid b-line/60%'>
-          <Button size='small' className='flex gap-4px items-center c-font! hover:c-primary!' onClick={openSettings}>
-            <Icon icon='i-icons-setting' className='size-16px!' />
-            <span className='text-14px font-500'>设置</span>
-          </Button>
-        </div>
-      </div>
-      <div className='flex-1 w-full of-hidden pt-30px'>
-        <button onClick={getSettings}>获取设置</button>
-          <button onClick={openSettings}>打开设置页</button>
-        </div>
-      </div>
+      <Layout />
     </MUIThemeProvider>
   );
 }
--- src/workbench/layout/Layout.tsx
+++ src/workbench/layout/Layout.tsx
@@ -1,23 +1,38 @@
-import { ReactNode } from 'react';
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import { Icon } from '@/components/Icon';
+import { WebviewWindow } from '@tauri-apps/api/webviewWindow';
+import cn from 'classnames';
+import { platform } from '@tauri-apps/plugin-os';
 
-interface LayoutProps {
-  children: ReactNode;
-  openSettings: () => void;
-  getSettings: () => void;
-}
+export default function Layout() {
+  const getSettings = () => {
+    const settings = localStorage.getItem('settings');
+    console.log(settings);
+  };
 
-export default function Layout({ children, openSettings, getSettings }: LayoutProps) {
+  const openSettings = async () => {
+    const settingWindow = await WebviewWindow.getByLabel('setting');
+    settingWindow?.setAlwaysOnTop(true);
+    settingWindow?.show();
+    settingWindow?.setFocus();
+    settingWindow?.setAlwaysOnTop(false);
+  };
+
   return (
     <div className='w-full h-full flex'>
       <div className='w-full h-25px shrink-0 absolute top-0 left-0' data-tauri-drag-region></div>
-      <div className='w-240px h-full flex flex-col of-hidden b-r-1 b-solid b-line/60% pt-10px'>
-        <div className='h-50px b-b-1 b-solid b-line/60% flex items-center justify-center'>
-          LOGO
+      <div className='w-220px h-full flex flex-col of-hidden b-r-1 b-solid b-line/60% pt-16px'>
+        <div className='px-16px flex items-center gap-8px'>
+          <Icon icon='i-icons-search' className='size-24px! absolute left-22px c-font_1' />
+          <TextField
+            size='small'
+            variant='outlined'
+            placeholder="搜索会话"
+            className='h-40px [&_input]:pl-35px'
+          />
         </div>
         <div className='flex-1 h-full p-16px'>
-          123
+          会话列表
         </div>
         <div className='h-40px px-16px flex items-center b-t-1 b-solid b-line/60%'>
           <Button size='small' className='flex gap-4px items-center' onClick={openSettings}>
@@ -26,10 +41,8 @@ export default function Layout({ children, openSettings, getSettings }: LayoutPr
           </Button>
         </div>
       </div>
-      <div className='flex-1 w-full of-hidden pt-30px'>
-        <button onClick={getSettings}>获取设置</button>
-        <button onClick={openSettings}>打开设置页</button>
-        {children}
+      <div className='flex-1 w-full of-hidden'>
+        聊天页面
       </div>
     </div>
   );
